cff172ee51a4bfd2b046f5e6675ec0d2fe05574d
vincentlie18 <49356938+vincentlie18@users.noreply.github.com>
2019-11-21 20:24:20 +0700

    Update loadgame.c

--- program/loadgame.c
+++ program/loadgame.c
@@ -6,12 +6,12 @@ int main()
    int num;
    FILE *fptr;
    if ((fptr = fopen("./program.txt","r")) == NULL) { // Menggunakan path relative, bisa juga diganti menjadi path tertentu
-       printf("Error! opening file");
+       printf("Error opening file!");
        // Program ada jika file pointernya mengembalikan NULL.
        exit(1);
    }
    fscanf(fptr,"%d", &num);
-   printf("Value of n=%d", num);
+   printf("Value of n=%d", num); // Jangan lupa dihapus karena hanya untuk tes output program.txt nya
    fclose(fptr); 
    return 0;
 }
