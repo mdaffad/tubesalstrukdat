0017e65d08a49c97318bfafc17474ef60bcd58ab
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2019-11-22 13:23:48 +0700

    driver queue

---
+++ program/queue/driver/driver_queue.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "../queue.h"
+#include "../../boolean/boolean.h"
+
+#include <stdlib.h>
+
+int main(){
+	Queue Q;
+
+	QCreateEmpty(&Q,2);
+	printf("isEmpty? : %d\n", IsLEmpty(Q));
+
+	printf("Add 1, ");
+	Add(&Q, 1);
+    printf("Add 2, ");
+	Add(&Q, 2);
+	printf("\n");
+    printf("isFull? : %d\n", IsQFull(Q));
+    printf("QNBElmt? : %d\n", QNBElmt(Q));
+
+    int X;
+    Del (&Q, &X);
+    printf("Del? : %d\n", X);
+    Del (&Q, &X);
+    printf("Del? : %d\n", X);
+
+
+}
