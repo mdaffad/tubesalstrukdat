b7d653d51043a63550eff15934ca4a3ca37dd5de
Steve <stevebezalel215@gmail.com>
2019-11-24 22:26:00 +0700

    fix driver arraydin

--- program/arraydinpos/driver/driver_arraydinpos.c
+++ program/arraydinpos/driver/driver_arraydinpos.c
@@ -18,68 +18,69 @@ void menu() {
 int main() {
     int input;
     TabBangunan T;
-    TabBangunan T1;
     IdxType i;
+    int tmp;
     menu();
     while(1) {
         printf("\n> ");
         scanf("%d", &input);
-        switch(c) {
+        switch(input) {
             case 1: {
                 printf("Mengirimkan banyaknya elemen efektif tabel, mengirimkan nol jika tabel kosong");
-                MakeEmpty(&T);
+                MakeEmpty(&T, 10);
                 printf("\nTabNbElmt: %d\n", TabNbElmt(T));
                 printf("\n");
                 break;
             }
             case 2: {
                 printf("Mengirimkan maksimum elemen yang dapat ditampung oleh tabel");
-                MakeEmpty(&T);
+                MakeEmpty(&T, 10);
                 printf("\nMaxElement: %d\n", MaxElement(T));
                 printf("\n");
                 break;
             }
             case 3: {
                 printf("Mengirimkan indeks elemen T pertama\n");
-                MakeEmpty(&T);
+                MakeEmpty(&T, 10);
                 printf("\nGetFirstIdx: %d\n", GetFirstIdx(T));
                 printf("\n");
                 break;
             }
             case 4: {
                 printf("Mengirimkan indeks elemen T terakhir\n");
-                MakeEmpty(&T);
+                MakeEmpty(&T, 10);
                 printf("\nGetLastIdx: %d\n", GetLastIdx(T));
                 printf("\n");
                 break;
             }
             case 5: {
                 printf("Mengirimkan true jika i adalah indeks yang valid utk ukuran tabel, yaitu antara indeks yang terdefinisi utk container\n");
-                MakeEmpty(&T);
-                printf("\nIsIdxTabValid: %d\n", IsIdxTabValid(T, i));
+                MakeEmpty(&T, 10);
+                printf("idx? : "); scanf("%d", &tmp);
+                printf("\nIsIdxTabValid: %d\n", IsIdxTabValid(T, tmp));
                 printf("\n");
                 break;
             }
             case 6: {
                 printf("Mengirimkan true jika i adalah indeks yang terdefinisi utk tabel, yaitu antara FirstIdx(T)..LastIdx(T)\n");
-                MakeEmpty(&T);
-                printf("\nIsIdxTabEff: %d\n", IsIdxTabEff(T, i));
+                MakeEmpty(&T, 10);
+                printf("idx? : "); scanf("%d", &tmp);
+                printf("\nIsIdxTabEff: %d\n", IsIdxTabEff(T, tmp));
                 printf("\n");
                 break;
             }
             case 7: {
                 printf("Mengirimkan true jika tabel T kosong, mengirimkan false jika tidak\n");
-                MakeEmpty(&T);
                 printf("\nIsTabEmpty: %d\n", IsTabEmpty(T));
-                printf("\nIsTabEmpty: %d\n", IsTabEmpty(T1));
-                printf("\n")
+                printf("\n");
+                break;
             }
             case 8: {
                 printf("Mengirimkan true jika tabel T penuh, mengirimkan false jika tidak\n");
-                MakeEmpty(&T);
+                MakeEmpty(&T, 10);
                 printf("\nIsTabFull: %d\n", IsTabFull(T));
-                printf("\nIsTabFull: %d\n", IsTabFull(T1));
                 printf("\n");
+                break;
             }
             default: {
                 printf("Not Available\n");
